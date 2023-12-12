d2a137ad6dc38432bdfac20c51195aa8bdc88c02
CodingJang <jjangga813@naver.com>
2023-12-12 11:44:33 +0900

    키오스크 2일차 오후 Main - 메인 함수 작성 Cart -payment EditList -> ShowList -print 항목 추가

--- src/main/main.kt
+++ src/main/main.kt
@@ -1,7 +1,66 @@
 package main
 
+import kotlin.random.Random
+
 fun main() {
     var menu = Menu()
-    var e = EditList()
-    e.showItemList(menu.single.items)
-}
+    var myCart = Cart()
+    val input = GetInput()
+    myCart.money = Random.nextInt(10000,100000)
+    var lists = ShowList()
+//    lists.showTypeList(menu.types)
+//    lists.showItemList(menu.single.items)
+//    println("내 소지금 ${myCart.money}")
+
+    println("[THE SPARTAN GARDEN] 에 당도한 것을 환영한다 !!!!")
+    println("아래의 메뉴판을 보고 원하는 바를 고하라 !!!!")
+
+    var shopping = true
+    while(shopping) // selectType
+    {
+        println("[Spartan Garden]")
+        lists.showTypeList(menu.types)
+        var t = input.inputNum(menu.types.size +2 ) -1 // order, cancel
+        when(t){
+            // Type 안에 선택을 한 경우
+            in 0 until menu.types.size-> {
+                println("[ ${menu.types[t].name} 메뉴 ]")
+                lists.showItemList(menu.types[t].items)
+                var i = input.inputNum(menu.types[t].items.size+1) - 1
+                when(i){
+                    in 0 until menu.types[t].items.size->{
+                        println("좋은 선택이다 !!!!")
+                        myCart.addItem(menu.types[t].items[i])
+                    }
+                    else -> println("신중히 살펴보아라 !!!!")
+                }
+            }
+
+            // order
+            menu.types.size -> {
+                myCart.showCart()
+                println("이대로 결제를 진행해드릴까요 ?")
+                println("1. 주문하기")
+                println("2. 주문취소")
+                println("3. 돌아가기")
+                var o = input.inputNum(2)
+                when(o) {
+                    // 주문
+                    1 -> {
+                        var result = myCart.payment()
+                        if(result == 1)
+                            myCart.items = arrayListOf()
+                    }
+                    // 취소
+                    2 -> println("신중히 살펴보아라 !!!!")
+                }
+            }
+
+            // cancel
+            menu.types.size+1 -> {
+                println("다음에 또 오도록 !!!!")
+                shopping=false
+            }
+        } // when(t)
+    } // while(shopping)
+} // main
